12554545ff1eb428e93922d7a913e0c49433d13b
Nitigya Joshi <77828908+nitigya-joshi@users.noreply.github.com>
2023-04-19 15:16:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,50 +1,53 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const userRoutes = require('./routes/userRoutes');
-const doctorRoutes = require('./routes/doctorRoutes');
-const contactRoutes = require('./routes/apiRoutes');
-const cookieParser = require('cookie-parser');
-const swaggerJSDoc = require('swagger-jsdoc');
-const swaggerUI = require('swagger-ui-express');
-const cors = require('cors')
+const express = require("express");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const doctorRoutes = require("./routes/doctorRoutes");
+const contactRoutes = require("./routes/apiRoutes");
+const cookieParser = require("cookie-parser");
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
+const cors = require("cors");
 const port = 3000;
 const app = express();
-const path = require('path');
-const morgan = require('morgan')
+const path = require("path");
+const morgan = require("morgan");
 
 connectDB();
-app.use(cors({ credentials: true, origin: 'http://localhost:3001' }))
+app.use(
+	cors({
+		credentials: true,
+		origin: "https://vaidya-healthcare-grp41.vercel.app/",
+	})
+);
 app.use(cookieParser());
-app.use(express.json({ limit: '8mb' }))
-app.use(morgan('combined'))
+app.use(express.json({ limit: "8mb" }));
+app.use(morgan("combined"));
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const swaggerOptions = {
-    definition: {
-        openapi: "3.0.0",
-        info: {
-            title: "Vaidya Healthcare API endpoints",
-            version: "1.0.0",
-        },
-        servers: [
-            {
-                url: "http://localhost:3000/api",
-            },
-        ],
-    },
-    apis: ["./routes/*.js"],
-    url: "http://localhost:3000/",
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "Vaidya Healthcare API endpoints",
+			version: "1.0.0",
+		},
+		servers: [
+			{
+				url: "https://vaidya-healthcare-grp41.vercel.app/",
+			},
+		],
+	},
+	apis: ["./routes/*.js"],
+	url: "https://vaidya-healthcare-grp41.vercel.app/",
 };
 const swaggerDocs = swaggerJSDoc(swaggerOptions);
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
-app.use('/api/users', userRoutes);
-app.use('/api/doctors', doctorRoutes);
-app.use('/api', contactRoutes);
-
+app.use("/api/users", userRoutes);
+app.use("/api/doctors", doctorRoutes);
+app.use("/api", contactRoutes);
 
 app.listen(port, () => {
-    console.log('Server is up on port ' + port)
-})
-
+	console.log("Server is up on port " + port);
+});
